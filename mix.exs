21899d2bf9f04ac76d4f2ef507da6f587e721772
Roman Heinrich <roman.heinrich@gmail.com>
2021-09-06 15:15:16 +0200

    Add ex_doc for docs generation on publishing to Hex.pm

--- mix.exs
+++ mix.exs
@@ -32,6 +32,8 @@ defmodule ExSqlean.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
   end
 end
